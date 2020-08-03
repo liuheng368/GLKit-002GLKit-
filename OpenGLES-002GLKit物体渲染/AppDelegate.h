d27672adc1d003f8e42001570e88af04cae2e104
Henry <lhappledev@163.com>
2020-08-03 22:57:08 +0800

    Initial Commit

---
+++ OpenGLES-002GLKit物体渲染/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLES-002GLKit物体渲染
+//
+//  Created by Henry on 2020/8/3.
+//  Copyright © 2020 Henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGLES-002GLKit物体渲染/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  OpenGLES-002GLKit物体渲染
+//
+//  Created by Henry on 2020/8/3.
+//  Copyright © 2020 Henry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
